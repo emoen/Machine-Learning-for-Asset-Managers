1e21069c708cb07c67a2279ab177834026d90869
Moen Endre <endre.moen@imr.no>
2020-07-06 15:56:02 +0200

    error in errPDFs()

--- marcenko_pastur_pdf.py
+++ marcenko_pastur_pdf.py
@@ -56,17 +56,17 @@ def cov2corr(cov):
 #snippet 2.4 - fitting the marcenko-pastur pdf - find variance
 #Fit error
 def errPDFs(var, eVal, q, bWidth, pts=1000):
-    pdf0 = mpPDF(var, q, pts) #theoretical pdf
+    pdf0 = mpPDF(var[0], q, pts) #theoretical pdf
     pdf1 = fitKDE(eVal, bWidth, x=pdf0.index.values) #empirical pdf
     sse = np.sum((pdf1-pdf0)**2)
     print("sse:"+str(sse))
-    return sse
+    return sse 
     
 #find max random eVal by fitting Marcenko's dist
 def findMaxEval(eVal, w, bWidth):
-    bnds = ((float(1e5/10000000000), float(0.99999*-1)),)
-    print(bnds)
-    out = minimize(lambda*x: errPDFs(*x), .5, args=(eVal, q, bWidth), bounds=bnds)
+    out = minimize(lambda *x: errPDFs(*x), .5, args=(eVal, q, bWidth), bounds=((1E-5, 1-1E-5),))
+          #minimize(lambda *x: errPDFs(*x), np.atleast_1d(.5), args=(eVal, q, bWidth), bounds=bnds)
+    print(out)
     print(out['success'])
     print(out['x'][0])
     if out['success']: var = out['x'][0]
@@ -76,34 +76,34 @@ def findMaxEval(eVal, w, bWidth):
     
 if __name__ == '__main__':
     # code snippet 2.2 - marcenko-pastur pdf explains eigenvalues of random matrix x
-    N = 1000
-    T = 10000
-    x = np.random.normal(0, 1, size = (T, N))
-    cor = np.corrcoef(x, rowvar=0) # cor.shape = (1000,1000). If rowvar=1 - row represents a var, with observations in the columns.
-    eVal0 , eVec0 = getPCA( cor ) 
-    pdf0 = mpPDF(1., q=x.shape[0]/float(x.shape[1]), pts=N)
-    pdf1 = fitKDE(np.diag(eVal0), bWidth=.005) #empirical pdf
-        
-    # code snippet 2.3 - random matrix with signal
-    alpha, nCols, nFact, q = .995, 1000, 100, 10
-    cov = np.cov(np.random.normal(size=(nCols*q, nCols)), rowvar=0) #size = (1000*10,1000)
-    cov = alpha*cov+(1-alpha)*getRndCov(nCols, nFact) # noise + signal
-    corr0 = cov2corr(cov)
-    eVal01, eVec01 = getPCA(corr0)
-    pdf2 = fitKDE(np.diag(eVal01), bWidth=.15) #empirical pdf
+N = 1000
+T = 10000
+x = np.random.normal(0, 1, size = (T, N))
+cor = np.corrcoef(x, rowvar=0) # cor.shape = (1000,1000). If rowvar=1 - row represents a var, with observations in the columns.
+eVal0 , eVec0 = getPCA( cor ) 
+pdf0 = mpPDF(1., q=x.shape[0]/float(x.shape[1]), pts=N)
+pdf1 = fitKDE(np.diag(eVal0), bWidth=.005) #empirical pdf
+    
+# code snippet 2.3 - random matrix with signal
+alpha, nCols, nFact, q = .995, 1000, 100, 10
+cov = np.cov(np.random.normal(size=(nCols*q, nCols)), rowvar=0) #size = (1000*10,1000)
+cov = alpha*cov+(1-alpha)*getRndCov(nCols, nFact) # noise + signal
+corr0 = cov2corr(cov)
+eVal01, eVec01 = getPCA(corr0)
+pdf2 = fitKDE(np.diag(eVal01), bWidth=.15) #empirical pdf
 
-    # Figure 2.1 Plot empirical:KDE and Marcenko-Pastur, and histogram
-    fig = plt.figure()
-    ax  = fig.add_subplot(111)
-    bins = 50
-    ax.hist(np.diag(eVal0), normed = True, bins=50) # Histogram the eigenvalues
+# Figure 2.1 Plot empirical:KDE and Marcenko-Pastur, and histogram
+fig = plt.figure()
+ax  = fig.add_subplot(111)
+bins = 50
+ax.hist(np.diag(eVal0), normed = True, bins=50) # Histogram the eigenvalues
 
-    plt.plot(pdf0.keys(), pdf0, color='r', label="Marcenko-Pastur pdf")
-    plt.plot(pdf1.keys(), pdf1, color='g', label="Empirical:KDE")
-    #plt.plot(x_range, pdf2, color='b', label="Eigenvalues of random-matrix with signal")
-    plt.legend(loc="upper right")
-    plt.show()
+plt.plot(pdf0.keys(), pdf0, color='r', label="Marcenko-Pastur pdf")
+plt.plot(pdf1.keys(), pdf1, color='g', label="Empirical:KDE")
+#plt.plot(x_range, pdf2, color='b', label="Eigenvalues of random-matrix with signal")
+plt.legend(loc="upper right")
+plt.show()
     
     # code snippet 2.4 
-eMax0, var0 = findMaxEval(np.diag(eVal0), q, bWidth=.01)
+eMax0, var0 = findMaxEval(np.diag(eVal01), q, bWidth=.01)
 nFacts0 = eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
