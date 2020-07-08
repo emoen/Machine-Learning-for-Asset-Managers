788fe9fb9bb9b9d6df70a42db9bbc295a19082bb
Moen Endre <endre.moen@imr.no>
2020-07-08 14:27:01 +0200

    add timeseries to file, plot fig 2.3

--- Covariance_matrix.py
+++ Covariance_matrix.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import yfinance as yf
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -26,8 +27,76 @@ import marcenko_pastur_pdf as mp
 #       alternative vectors of expected returns from a Normal distribution
 #       with mean 10% and std 10%
 #    c) Compute the variance of the errors agains the mean efficient frontier.
-def covariance_of_OL():
+def get_OL_tickers_close(T=936, N=234):       
+    # N - num stocks in portfolio, T lookback time
+    ol = pd.read_csv('ol_ticker.csv', sep='\t', header=None)
+    ticker_names = ol[0]
+    S = np.empty([T, N])
+    covariance_matrix = np.empty([T, N])
+    portfolio_name = [ [ None ] for x in range( N ) ]
+    ticker_adder = 0
+    for i in range(0, len(ticker_names)):  #len(ticker_names)):  # 46
+        ticker = ticker_names[i]
+        print(ticker)
+        ol_ticker = ticker + '.ol'
+        df = yf.Ticker(ol_ticker)
+        #'shortName' in df.info and
+        try:
+            ticker_df = df.history(period="max")
+            if ticker=='EMAS': print("****EMAS******")
+            if ticker=='AVM': print("****AVM*********")
+            if len(df.history(period="max")) > T and ticker!='EMAS' and ticker != 'AVM':  # only read tickers with more than 30 days history
+                #1.Stock Data
+                S[:,ticker_adder] = ticker_df['Close'][-T:].values # inserted from oldest tick to newest tick
+                portfolio_name[ticker_adder] = ol_ticker
+                ticker_adder += 1
+            else:
+                print("no data for ticker:" + ticker)
+        except ValueError:
+            print("no history:"+ol_ticker)
     
+    return S, portfolio_name
+    
+def denoise_OL(S, portfolio_name):
+    
+    np.argwhere( np.isnan(S) )
+    S = S[:,0:183]
+    N = 183
+    portfolio_name = portfolio_name[0:183]
+    
+    # cor.shape = (1000,1000). If rowvar=1 - row represents a var, with observations in the columns.
+    cor = np.corrcoef(S, rowvar=0) 
+    eVal0 , eVec0 = mp.getPCA( cor ) 
+    print(np.argwhere(np.isnan(np.diag(eVal0))))
+    pdf0 = mp.mpPDF(1., q=S.shape[0]/float(S.shape[1]), pts=N)
+    pdf1 = mp.fitKDE( np.diag(eVal0), bWidth=.005) #empirical pdf
+
+    fig = plt.figure()
+    ax  = fig.add_subplot(111)
+    bins = 50
+    ax.hist(np.diag(eVal0), normed = True, bins=50) 
+ 
+    #plt.plot(pdf1.keys(), pdf1, color='g') #no point in drawing this
+    plt.plot(pdf0.keys(), pdf0, color='r')
+    plt.show()
+        
+    # code snippet 2.4 
+    eMax0, var0 = mp.findMaxEval(np.diag(eVal0), q, bWidth=.01)
+    nFacts0 = eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
+    
+    # code snippet 2.5
+    corr1 = mp.denoisedCorr(eVal0, eVec0, nFacts0)
+    eVal1, eVec1 = mp.getPCA(corr1)
+    
+
+def correlation_from_covariance(covariance):
+    v = np.sqrt(np.diag(covariance))
+    outer_v = np.outer(v, v)
+    correlation = covariance / outer_v
+    correlation[covariance == 0] = 0
+    return correlation
+
+def calculate_correlation(S, T=936, N=234):
     """ Create covariance matrix 
     >>> import numpy as np
     >>> n = 3
@@ -53,39 +122,10 @@ def covariance_of_OL():
     >>> covariance = covariance.astype('float32')
     >>> correlation = np.divide(covariance, stds_m)
     >>> np.testing.assert_array_equal(correlation, np.corrcoef(S))
+    >>> print(correlation)
+    >>> print(correlation_from_covariance(covariance))
     """
-        
-    ol = pd.read_csv('ol_ticker.csv', sep='\t', header=None)
-    ticker_name = ol[0]
-    n=234# num stocks in portfolio
-    T=936
-    S = np.empty([n, T])
-    covariance_matrix = np.empty([n, T])
-    portfolio_name = [ [ None ] for x in range( n ) ]
-    mean_stonks = np.empty([n])
-    ticker_adder = 0
-    for i in range(0, len(ticker_name)):  #len(ticker_name)):  # 46
-        ticker = ticker_name[i]
-        print(ticker)
-        ol_ticker = ticker + '.ol'
-        df = yf.Ticker(ol_ticker)
-        #'shortName' in df.info and
-        try:
-            ticker_df = df.history(period="max")
-            if ticker=='EMAS': print("****EMAS******")
-            if ticker=='AVM': print("****AVM*********")
-            if len(df.history(period="max")) > T and ticker!='EMAS' and ticker != 'AVM':  # only read tickers with more than 30 days history
-                #1.Stock Data
-                S[ticker_adder] = ticker_df['Close'][-T:].values
-                portfolio_name[ticker_adder] = ol_ticker
-                ticker_adder += 1
-            else:
-                print("no data for ticker:" + ticker)
-        except ValueError:
-            print("no history:"+ol_ticker)
-    
-    np.argwhere(np.isnan(S))
-    
+
     #2.Average Price Of Stock
     M = np.sum(S, axis=1)/T #sum along row
     #3.Demeaning The Prices
@@ -94,7 +134,7 @@ def covariance_of_OL():
     #Once we have the de-meaned price series, we establish the
     #covariance of different stocks by multiplying the transpose of
     #the de-meaned price series with itself and divide it by 'm'
-    covariance = (np.dot(de_meaned_S, de_meaned_S.T))/(n-1)
+    covariance = (np.dot(de_meaned_S, de_meaned_S.T))/(N-1)
     # The eigen-values of the covariance matrix is distributed like Marcenko-Pasture dist.
     #any any eigenvalues outside distribution is signal else noise.
     
@@ -106,40 +146,17 @@ def covariance_of_OL():
     eigenvalue, eigenvector = np.linalg.eig(np.corrcoef(S))
     eigenvalue = abs(eigenvalue)
     condition_num = max(eigenvalue) - min(eigenvalue)
-        
-    S = S[0:183]
-    n= 183
-    portfolio_name = portfolio_name[0:183]
-    
-    # cor.shape = (1000,1000). If rowvar=1 - row represents a var, with observations in the columns.
-cor = np.corrcoef(S, rowvar=1) 
-eVal0 , eVec0 = mp.getPCA( cor ) 
-eVal0 = np.diag(eVal0)
-print(np.argwhere(np.isnan(eVal0)))
-pdf0 = mp.mpPDF(1., q=S.shape[1]/float(S.shape[0]), pts=n)
-pdf1 = mp.fitKDE( eVal0, bWidth=.005) #empirical pdf
-
-fig = plt.figure()
-ax  = fig.add_subplot(111)
-bins = 50
-ax.hist(eVal0, normed = True, bins=50) 
-
-#plt.plot(pdf1.keys(), pdf1, color='g')
-plt.plot(pdf0.keys(), pdf0, color='r')
-plt.show()
-    
-eMax0, var0 = findMaxEval(eVal0, q, bWidth=.01)
-    nFacts0 = eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
-
-def correlation_from_covariance(covariance):
-    v = np.sqrt(np.diag(covariance))
-    outer_v = np.outer(v, v)
-    correlation = covariance / outer_v
-    correlation[covariance == 0] = 0
-    return correlation
     
 if __name__ == '__main__':
-    covariance_of_OL()
+    S = np.loadtxt('ol183.csv', delimiter=',')
+    portfolio_name = pd.read_csv('ol_names.csv', delimiter=',',header=None)[0].tolist()
+    if S.shape[0] <1:
+        S, portfolio_name = get_OL_tickers_close()
+        np.savetxt('ol183.csv', S, delimiter=',')
+        np.savetxt('ol_names.csv', np.asarray(portfolio_name), delimiter=',', fmt='%s')
+        
+    calculate_correlation(S)
+    denoise_OL(S, portfolio_name)
     
-    #import doctest
-    #doctest.testmod()
+    import doctest
+    doctest.testmod()
--- marcenko_pastur_pdf.py
+++ marcenko_pastur_pdf.py
@@ -12,17 +12,17 @@ def mpPDF(var, q, pts):
     eVal = np.linspace(eMin, eMax, pts) #Return evenly spaced numbers over a specified interval. eVal='lambda'
     #Note: 1.0/2*2 = 1.0 not 0.25=1.0/(2*2)
     pdf = q/(2*np.pi*var*eVal)*((eMax-eVal)*(eVal-eMin))**.5 #np.allclose(np.flip((eMax-eVal)), (eVal-eMin))==True
-    pdf=pd.Series(pdf, index=eVal)
+    pdf = pd.Series(pdf, index=eVal)
     return pdf
 
 #snippet 2.2
 #Test Marcenko-Pastur Thm
 def getPCA(matrix):
     # Get eVal, eVec from a Hermitian matrix
-    eVal, eVec=np.linalg.eig(matrix) #complex Hermitian (conjugate symmetric) or a real symmetric matrix.
-    indices=eVal.argsort()[::-1] #arguments for sorting eval desc
+    eVal, eVec = np.linalg.eig(matrix) #complex Hermitian (conjugate symmetric) or a real symmetric matrix.
+    indices = eVal.argsort()[::-1] #arguments for sorting eval desc
     eVal,eVec = eVal[indices],eVec[:,indices]
-    eVal=np.diagflat(eVal) # identity matrix with eigenvalues as diagonal
+    eVal = np.diagflat(eVal) # identity matrix with eigenvalues as diagonal
     return eVal,eVec
     
 def fitKDE(obs, bWidth=.15, kernel='gaussian', x=None):
@@ -35,7 +35,7 @@ def fitKDE(obs, bWidth=.15, kernel='gaussian', x=None):
     if x is None: x = np.unique(obs).reshape(-1,1)
     print(len(x.shape))
     if len(x.shape) == 1: x = x.reshape(-1,1)
-    logProb=kde.score_samples(x) # log(density)
+    logProb = kde.score_samples(x) # log(density)
     pdf = pd.Series(np.exp(logProb), index=x.flatten())
     return pdf
 
@@ -56,7 +56,8 @@ def cov2corr(cov):
 #snippet 2.4 - fitting the marcenko-pastur pdf - find variance
 #Fit error
 def errPDFs(var, eVal, q, bWidth, pts=1000):
-    pdf0 = mpPDF(var[0], q, pts) #theoretical pdf
+    var = var[0]
+    pdf0 = mpPDF(var, q, pts) #theoretical pdf
     pdf1 = fitKDE(eVal, bWidth, x=pdf0.index.values) #empirical pdf
     sse = np.sum((pdf1-pdf0)**2)
     print("sse:"+str(sse))
@@ -64,9 +65,7 @@ def errPDFs(var, eVal, q, bWidth, pts=1000):
     
 #find max random eVal by fitting Marcenko's dist
 def findMaxEval(eVal, w, bWidth):
-    out = minimize(lambda *x: errPDFs(*x), .5, args=(eVal, q, bWidth), bounds=((1E-5, 1-1E-5),))
-    print(out)
-    print(out['success'])
+    out = minimize(lambda *x: errPDFs(*x), x0=np.array(0.5), args=(eVal, q, bWidth), bounds=((1E-5, 1-1E-5),))
     print(out['x'][0])
     if out['success']: var = out['x'][0]
     else: var=1
@@ -76,7 +75,7 @@ def findMaxEval(eVal, w, bWidth):
 #snippet 2.5 - denoising by constant residual eigenvalue
 # Remove noise from corr by fixing random eigenvalue
 def denoisedCorr(eVal, eVec, nFacts):
-    eval_ = np.diag(eVal).copy()
+    eVal_ = np.diag(eVal).copy()
     eVal_[nFacts:] = eVal_[nFacts:].sum()/float(eVal_.shape[0] - nFacts)
     eVal_ = np.diag(eVal_)
     corr1 = np.dot(eVec, eVal_).dot(eVec.T)
@@ -113,11 +112,20 @@ if __name__ == '__main__':
     plt.legend(loc="upper right")
     plt.show()
     
-    # code snippet 2.4 
+    # code snippet 2.4 - fitting the marcenko-pastur pdf - find variance
     eMax0, var0 = findMaxEval(np.diag(eVal01), q, bWidth=.01)
-    nFacts0 = eVal0.shape[0]-np.diag(eVal0)[::-1].searchsorted(eMax0)
-    
-    # code snippet 2.5
-    corr1 = denoisedCorr(eVal0, eVec0, nFacts0)
+    nFacts0 = eVal01.shape[0]-np.diag(eVal01)[::-1].searchsorted(eMax0)
+
+    # code snippet 2.5 - denoising by constant residual eigenvalue
+    corr1 = denoisedCorr(eVal01, eVec0, nFacts0)
     eVal1, eVec1 = getPCA(corr1)
+
+    denoised_eigenvalue = np.diag(eVal1)
+    eigenvalue_prior = np.diag(eVal01)
+    plt.plot(range(0, len(denoised_eigenvalue)), np.log(denoised_eigenvalue), color='r', label="Denoised eigen-function")
+    plt.plot(range(0, len(eigenvalue_prior)), np.log(eigenvalue_prior), color='g', label="Original eigen-function")
+    plt.xlabel("Eigenvalue number")
+    plt.ylabel("Eigenvalue (log-scale)")
+    plt.legend(loc="upper right")
+    plt.show()
     
