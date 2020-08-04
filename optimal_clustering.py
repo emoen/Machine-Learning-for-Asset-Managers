33a3d0c35d94c42d65557c580f041495c20406b6
Moen Endre <endre.moen@imr.no>
2020-08-04 14:08:31 +0200

    document simulation of random block matrix creation

--- optimal_clustering.py
+++ optimal_clustering.py
@@ -65,54 +65,71 @@ def clusterKMeansTop(corr0, maxNumClusters=None, n_init=10):
         tStatMean=np.mean([clusterTstats[i] for i in redoClusters])
         corr2, clstrs2, silh2=clusterKmeansTop(corrTmp, maxNumClusters=min(maxNumClusters, corrTmp,.shape[1]-1),n_init=n_init)
         
-#codesnippet 4.3
-#Random block correlation matrix creation
+#codesnippet 4.3 - utility for monte-carlo simulation
+# Random block correlation matrix creation
+# Simulates a time-series of atleast 100 elements. 
+# So each column is highly correlated for small sigma and less correlated for large sigma (standard deviation)
+#
+# two matrixes of N(0,sigma^2) rv added which results in variance=2*sigma^2
 def getCovSub(nObs, nCols, sigma, random_state=None):
     #sub correl matrix
     rng = check_random_state(random_state)
-    if nCols==1:
+    if nCols == 1:
         return np.ones((1,1))
     ar0 = rng.normal(size=(nObs, 1)) #array of normal rv
-    ar0 = np.repeat(ar0, nCols, axis=1) #matrix of columns repeating rv
-    ar0 += rng.normal(loc=0, scale=sigma, size=ar0.shape) #add N(0,1) to rv
-    ar0 = np.cov(ar0, rowvar=False)
+    ar0 = np.repeat(ar0, nCols, axis=1) #matrix of columns repeating rv. Simulate time-series of at least 100 elements.
+    ar0 += rng.normal(loc=0, scale=sigma, size=ar0.shape) #add two rv X~Y~N(0,1), Z=X+Y~N(0+0, 1+1)=N(0,2)
+    ar0 = np.cov(ar0, rowvar=False) #ar0.shape = nCols x nCols
     return ar0
-    
+
+#generate a block random correlation matrix
+#
+# The last block in the matrix is going to be as large as possible
+# Controlling the size of the last block matrix can be done by inceasing minBlockSize
+# 
+# parts is the size of the blocks. If nCols, nBlocks, minBlockSize = 6,3,1
+# then parts = [1,1,4] resulting in 1x1, 1x1, 4x4 block-covariance-matrixes
+# If block > 1x1 matrix the diagonal is 2 or 2*sigma as the variance of
+# covariance from getCovSub() is Z=X+Y => 2*sigma
 def getRndBlockCov(nCols, nBlocks, minBlockSize=1, sigma=1., random_state=None):
-    #generate a block random correlation matrix
-    rng=check_random_state(random_state)
+
+    rng = check_random_state(random_state)
     parts = rng.choice(range(1, nCols-(minBlockSize-1)*nBlocks), nBlocks-1, replace=False)
     parts.sort()
-    parts=np.append(parts, nCols-(minBlockSize-1)*nBlocks) #add largest number
-    parts=np.append(parts[0], np.diff(parts)-1+minBlockSize)
+    parts = np.append(parts, nCols-(minBlockSize-1)*nBlocks) #add nCols to list of parts, unless minBlockSize>1
+    parts = np.append(parts[0], np.diff(parts)-1+minBlockSize)
     cov=None
     for nCols_ in parts:
-        cov_=getCovSub(int(max(nCols_*(nCols_+1)/2., 100)), nCols_, sigma, random_state=rng)
+        cov_ = getCovSub(int(max(nCols_*(nCols_+1)/2., 100)), nCols_, sigma, random_state=rng)
         if cov is None:
-            cov=cov_.copy()
+            cov = cov_.copy()
         else: 
-            cov=block_diag(cov, cov_)
+            cov = block_diag(cov, cov_) #list of square matrix on larger matrix on the diagonal
     
     return cov
 
+# add two random covariance matrixes and return the correlation matrix as a dataframe. 
+#
+# The first covariance matrix consists of nBlocks
+# and the second matrix consists of 1 block - which adds noice.
+# Note: noice is also added in each block matrix. Why is noice added 2 times?
 def randomBlockCorr(nCols, nBlocks, random_state=None, minBlockSize=1):
     #Form block corr
     rng = check_random_state(random_state)
     
-    cov0 = getRndBlockCov(nCols, nBlocks, 
-        minBlockSize=minBlockSize, sigma=.5, random_state=rng)
-    cov1 = getRndBlockCov(nCols, 1, minBlockSize=minBlockSize, sigma=1., random_state=rng)#add noise
+    cov0 = getRndBlockCov(nCols, nBlocks, minBlockSize=minBlockSize, sigma=.5, random_state=rng)
+    cov1 = getRndBlockCov(nCols, 1, minBlockSize=minBlockSize, sigma=1., random_state=rng) #add noise
     cov0 += cov1
     corr0 = mp.cov2corr(cov0)
     corr0 = pd.DataFrame(corr0)
     return corr0
     
 if __name__ == '__main__':
-    nCols, nBlocks = 10, 2
+    nCols, nBlocks = 6, 3
     nObs = 8
     sigma = 1.
     corr0 = randomBlockCorr(nCols, nBlocks)
-    testGetCovSub = getCovSub(nObs, nCols, sigma, random_state=None) # 10x10 matrix
+    testGetCovSub = getCovSub(nObs, nCols, sigma, random_state=None) 
     tmp = testGetCovSub.flatten()
     
     fig = plt.figure()
