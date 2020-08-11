8dac87e7981973f144baa85798d83a05610faa07
Moen Endre <endre.moen@imr.no>
2020-08-11 12:48:12 +0200

    scatterplot t_values in chap 5

--- ch5_financial_labels.py
+++ ch5_financial_labels.py
@@ -16,6 +16,13 @@ def tValLinR(close):
     return ols.tvalues[1]
     
 #code snippet 5.2
+'''
+    - molecule - index of observations we wish to labels. 
+    - close - which is the time series of x_t
+    - span - is the set of values of L (look forward period) that the algorithm will try.
+    The L that maximizes |tHat_B_1| is choosen - which is the look-forward period 
+    with the most significant trend.
+'''
 def getBinsFromTrend(molecule, close, span):
     '''
     Derive labels from the sign of t-value of trend line
@@ -47,7 +54,18 @@ if __name__ == '__main__':
     df0 = pd.Series(np.random.normal(0, .1, 100)).cumsum()
     df0 += np.sin(np.linspace(0, 10, df0.shape[0]))
     df1 = getBinsFromTrend(df0.index, df0, [3,10,1]) #[3,10,1] = range(3,10)
-    plt.scatter(df1.index, df0.loc[df1.index].values, c=df1['bin'].values, cmap='viridis')
+
+    #normalise t-values to -1, 1
+    tValues = df1['tVal'].values
+    minusArgs = [i for i in range(0, len(tValues)) if tValues[i] < 0]
+    tValues[minusArgs] = tValues[minusArgs] / (np.min(tValues)*(-1.0))
+
+    plus_one = [i for i in range(0, len(tValues)) if tValues[i] > 0]
+    tValues[plus_one] = tValues[plus_one] / np.max(tValues)
+
+
+    plt.scatter(df1.index, df0.loc[df1.index].values, c=tValues, cmap='viridis') #df1['tVal'].values, cmap='viridis')
+    plt.colorbar()
     plt.savefig('fig5.1.png')
     plt.clf()
     plt.close()
