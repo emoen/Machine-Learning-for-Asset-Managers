1e49cb13827cdb91e5dbd48f6ca75ac2ea71b618
Moen Endre <endre.moen@imr.no>
2020-08-21 09:32:11 +0200

    added clustered MDI and MDA

--- ch6_feature_importance_analysis.py
+++ ch6_feature_importance_analysis.py
@@ -39,15 +39,15 @@ def featImpMDI(fit, featNames):
 def featImpMDA(clf, X, y, n_splits=10):
     #feat importance based on OOS score reduction
     cvGen = KFold(n_splits=n_splits)
-    scr0, scr1=pd.Series(), pd.DataFrame(columns=X.columns)
+    scr0, scr1 = pd.Series(dtype='float64'), pd.DataFrame(columns=X.columns)
     for i, (train, test) in enumerate(cvGen.split(X=X)):
         x0, y0 = X.iloc[train, :], y.iloc[train]
         x1, y1 = X.iloc[test,:], y.iloc[test]
-        fit = clf.fit(X=X0, y=y0) # the fit occures
-        prob= fit.predict_proba(X1) #prediction before shuffles
-        scr0.loc[i]=-log_loss(y1, prob, labelslf.classes_)
+        fit = clf.fit(X=x0, y=y0) # the fit occures
+        prob= fit.predict_proba(x1) #prediction before shuffles
+        scr0.loc[i]=-log_loss(y1, prob, labels=clf.classes_)
         for j in X.columns:
-            X1_ = X1.copy(deep=True)
+            X1_ = x1.copy(deep=True)
             np.random.shuffle(X1_[j].values) #shuffle one columns
             prob = fit.predict_proba(X1_) #prediction after shuffle
             scr1.loc[i,j] = -log_loss(y1, prob, labels=clf.classes_)
@@ -55,8 +55,49 @@ def featImpMDA(clf, X, y, n_splits=10):
     imp = imp/(-1*scr1)
     imp=pd.concat({'mean':imp.mean(), 'std':imp.std()*imp.shape[0]**-.5}, axis=1) #CLT
     return imp
+    
+#code snippet 6.4 - clustered MDI
+def groupMeanStd(df0, clstrs):
+    out = pd.DataFrame(columns=['mean', 'std'])
+    for i, j in clstrs.iteritems():
+        df1 = df0[j].sum(axis=1)
+        out.loc['C_'+str(i), 'mean'] = df1.mean()
+        out.loc['C_'+str(i), 'std'] = df1.std()*df1.shape[0]**-.5
+    return out
 
-if __name__ == '__main__':    
+def featImpMDI_Clustered(fit, featNames, clstrs):
+    df0 = {i:tree.feature_importances_ for i, tree in enumerate(fit.estimator_)}
+    df0 = pd.DataFrame.from_dic(df0, orient='index')
+    df0.columns = featNames
+    df0 = df0.replace(0, np.nan) #because max_features=1
+    imp = grouMeanStd(df0, clstrs)
+    imp /= imp['mean'].sum()
+    return imp
+    
+#code snippet 6.5 - clustered MDA    
+def featImpMDA_Clustered(clf, X, y, clstrs, n_splits=10):
+    cvGen = KFold(n_splits=n_splits)
+    scr0, scr1 = pd.Series(dtype='float64'), pd.DataFrame(columns=clstrs.keys())
+    for i, (train, trest) in enumerate(cvGen.split(X=X)):
+        x0, y0, = X.iloc[train,:], y.iloc[train] 
+        x1, y1 = X.iloc[test, :], y.iloc[test]
+        fit = clt.fit(X=X0, y=y0)
+        prob=fit.predict_proba(X1)
+        scr0.loc[i] = -log_loss(y1, prob, labels=clf.classes_)
+        for j in scr1.columns:
+            X1_=X1.copy(deep=True)
+            for k in clstrs[j]:
+                np.random.shuffle(X1_[k].values) # shuffle clusters
+            prob=fit.predict_proba(X1_)
+            scr1.loc[i,j]=-log_loss(y1, prob, labels=clf.classes_)
+        imp=(-1*scr1).add(scr0,axis=0)
+        imp = imp/(-1*scr1)
+        imp = pd.concat({'mean:imp.mean(), std:imp.std()*imp.shape[0]**-.5}, axis=1)
+        imp.index=['C_'+str(i) for i in imp.index]
+    return imp
+
+if __name__ == '__main__':   
+    # 40 features, 5 informative features, 30 redundant, 5 noisy features
     X, y = getTestData(40, 5, 30, 10000, sigmaStd=.1)
     ols = sm1.Logit(y, X).fit()
     ols.summary()
@@ -65,7 +106,7 @@ if __name__ == '__main__':
     plt.show()
     
     #code snippet 6.2
-    X, y = getTestData(40, 4, 30, 10000, sigmaStd=.1)
+    X, y = getTestData(40, 5, 30, 10000, sigmaStd=.1)
     clf = DecisionTreeClassifier(criterion='entropy', 
                                  max_features=1, 
                                  class_weight='balanced', 
@@ -84,4 +125,25 @@ if __name__ == '__main__':
     plt.figure(figsize=(10, imp.shape[0] / 5))
     imp['mean'].plot(kind='barh', color='b', alpha=0.25, xerr=imp['std'], error_kw={'ecolor': 'r'})
     plt.title('Figure 6.2 Example of MDI results')
+    plt.show()
+    
+    #code snippet 6.3
+    X, y = getTestData(40, 5, 30, 10000, sigmaStd=.1)
+    clf = DecisionTreeClassifier(criterion='entropy', 
+                                 max_features=1, 
+                                 class_weight='balanced', 
+                                 min_weight_fraction_leaf=0)
+                                 
+    clf = BaggingClassifier(base_estimator=clf, 
+                          n_estimators=1000, 
+                          max_features=1., 
+                          max_samples=1., 
+                          oob_score=False)
+    fit = clf.fit(X,y)
+    imp = featImpMDA(clf, X, y, 10)
+    
+    imp.sort_values('mean', inplace=True)
+    plt.figure(figsize=(10, imp.shape[0] / 5))
+    imp['mean'].plot(kind='barh', color='b', alpha=0.25, xerr=imp['std'], error_kw={'ecolor': 'r'})
+    plt.title('Figure 6.3 Example of MDA results')
     plt.show()
