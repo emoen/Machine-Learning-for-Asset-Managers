50377e836dd54f3279e245d6ab0011db4e26b560
emoen <endremoen@hotmail.com>
2020-08-25 21:11:17 +0200

    block diag matrix

--- ch7_portfolio_construction.py
+++ ch7_portfolio_construction.py
@@ -8,6 +8,10 @@ import ch2_monte_carlo_experiment as mc
 # code snippet 7.1 - Composition of block-diagonal correlation matrices
 if __name__ == '__main__': 
     corr0 = mc.formBlockMatrix(2, 2, .5)
-    eval, eVec = np.linalg.eigh(corr0)
-    print(max(eVal)/min(eVal))
+    eVal, eVec = np.linalg.eigh(corr0)
+    matrix_condition_number = max(eVal)/min(eVal)
+    print(matrix_condition_number) 
+    
+    fig, ax = plt.subplots(figsize=(13,10))  
     sns.heatmap(corr0, cmap='viridis')
+    plt.show()
