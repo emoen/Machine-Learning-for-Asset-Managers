ca4093d42ffaabcf2f97baf7be361600dd2aaedb
Moen Endre <endre.moen@imr.no>
2020-08-28 12:59:33 +0200

    test and run NCO

--- ch7_portfolio_construction.py
+++ ch7_portfolio_construction.py
@@ -12,7 +12,30 @@ import ch4_optimal_clustering as oc
 
 def minVarPort(cov):
     return mc.optPort(cov, mu = None)
+
+# code snippet 7.6 - function implementing the NCO algorithm
+def optPort_nco(cov, mu=None, maxNumClusters=None):
+    cov = pd.DataFrame(cov)
+    if mu is not None:
+        mu = pd.Series(mu[:,0])
     
+    corr1 = mp.cov2corr(cov)
+    corr1, clstrs, _ = oc.clusterKMeansBase(corr1, maxNumClusters, n_init=10)
+    wIntra = pd.DataFrame(0, index=cov.index, columns=clstrs.keys())
+    for i in clstrs:
+        cov_ = cov.loc[clstrs[i], clstrs[i]].values
+        if mu is None:
+            mu_ = None
+        else: 
+            mu_ = mu.loc[clstrs[i]].values.reshape(-1,1)
+        wIntra.loc[clstrs[i],i] = mc.optPort(cov_, mu_).flatten()
+    
+    cov_ = wIntra.T.dot(np.dot(cov, wIntra)) #reduce covariance matrix
+    mu_ = (None if mu is None else wIntra.T.dot(mu))
+    wInter = pd.Series(mc.optPort(cov_, mu_).flatten(), index=cov_.index)
+    nco = wIntra.mul(wInter, axis=1).sum(axis=1).values.reshape(-1,1)
+    return nco
+   
 if __name__ == '__main__': 
     # code snippet 7.1 - Composition of block-diagonal correlation matric
     corr0 = mc.formBlockMatrix(2, 2, .5)
@@ -49,7 +72,7 @@ if __name__ == '__main__':
     
     # code snippet 7.4 - intracluster optimal allocations
     # step 2. compute intracluster allocations using the denoised cov matrix
-    wIntra = pd.DataFrame()
+    wIntra = pd.DataFrame(0, index=cov0.index, columns=clstrs.keys())
     for i in clstrs:
         wIntra.loc[clstrs[i], i] = minVarPort(cov1.loc[clstrs[i], clstrs[i]]).flatten()
         
@@ -58,7 +81,28 @@ if __name__ == '__main__':
     # code snippet 7.5 - intercluster optimal allocations
     # step 3. compute optimal intercluster allocations, usint the reduced covariance matrix
     # which is close to a diagonal matrix, so optimization problem is close to ideal case \ro =0
-    wInter = pd.Series(minVarPort(cov2)).flatten(), index=co2.index)
+    wInter = pd.Series(minVarPort(cov2).flatten(), index=cov2.index)
     wAll0 = wIntra.mul(wInter, axis=1).sum(axis=1).sort_index()
+
+    # step 4. Final allocations - dot-product of the intra-cluster and inter-cluster allocations 
+    #w_nco = w_intra_clusters.mul(w_inter_clusters, axis=1).sum(axis=1).values.reshape(-1, 1)
+    nco = wIntra.mul(wInter, axis=1).sum(axis=1).values.reshape(-1,1)
+    
+    # code snippet 7.7 - data-generating process
+    nBlocks, bSize, bCorr = 10, 50, .5
+    np.random.seed(0)
+    mu0, cov0 = mc.formTrueMatrix(nBlocks, bSize, bCorr)
+    
+    # code snippet 7.7 - Drawing an empirical vector of means and covariance matrix
+nObs, nSims, shrink, minVarPortf = 1000, 1000, False, True
+np.random.seed(0)
+w1 = pd.DataFrame(0, index=range(0, nSims), columns=range(0, len(cov1[1])))
+w1_d = pd.DataFrame(0, index=range(0, nSims), columns=range(0, len(cov1[1])))
+for i in range(0, nSims):
+    mu1, cov1 = mc.simCovMu(mu0, cov0, nObs, shrink=shrink)
+    if minVarPortf:
+        mu1 = None
+    w1.loc[i] = mc.optPort(cov1, mu1).flatten()
+    w1_d.loc[i] = optPort_nco(cov1, mu1, int(cov1.shape[0]/2)).flatten()
     
     
