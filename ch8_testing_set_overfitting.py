795d21fc7cdf8e3ff8a5004a854b5dc505c3d289
emoen <endremoen@hotmail.com>
2020-09-05 16:15:12 +0200

    code 8.2 - no reference to getDistDSR

--- ch8_testing_set_overfitting.py
+++ ch8_testing_set_overfitting.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm, percentileofscore
 
+
+
 # code snippet 8.1 - experimental validation of the false strategy theorem
 def getExpectedMaxSR(nTrials, meanSR, stdSR):
     #Expected max SR, controlling for SBuMT
@@ -24,7 +26,30 @@ def getDistMaxSR(nSims, nTrials, stdSR, meanSR):
         out_ = sr.max(axis=1).to_frame('max{SR}')
         out_['nTrials'] = nTrials_
         out = out.append(out_, ignore_index=True)
+    print(i)
     return out
+    
+# code snippet 8.2 - mean and standard deviation of the prediction errors
+def getMeanStdError(nSims0, nSims1, nTrials, stdSR=1, meanSR=0):
+    #compute standard deviation of errors per nTrials
+    #nTrials: [number of SR used to derive max{SR}]
+    #nSims0: number of max{SR} u{sed to estimate E[max{SR}]
+    #nSims1: number of errors on which std is computed
+    sr0=pd.Series({i:getExpectedMaxSR(i, meanSR, stdSR) for i in nTrials})
+    sr0 = sr0.to_frame('E[max{SR}]') 
+    sr0.index.name='nTrials'
+    err=pd.DataFrame()
+    for i in range(0, int(nSims1)):
+        sr1 = getDistDSR(nSims=1000, nTrials=nTrials, meanSR=0, stdSR=1)
+        sr1=sr1.groupby('nTrials').mean()
+        err_=sr0.join(sr1).reset_index()
+        err_['err'] = err_['max{SR}']/err_['E[max{SR}]']-1.
+        err=err.append(err_)
+    out = {'meanErr':err.groupby('nTrials')['err'].mean()}
+    out['stdErr'] = err.groupby('nTrials')['err'].std()
+    out = pd.DataFrame.from_dict(out, orient='columns')
+    return out
+
 
 if __name__ == '__main__': 
     nTrials = list(set(np.logspace(1, 6, 1000).astype(int)))
@@ -50,3 +75,8 @@ if __name__ == '__main__':
     imp['mean'].plot(kind='barh', color='b', alpha=0.25, xerr=imp['std'], error_kw={'ecolor': 'r'})
     plt.title('Figure 6.5 Clustered MDI')
     plt.show()
+    
+    # code snippet 8.2
+    nTrials=list(set(np.logspace(1, 6, 1000).astype(int)))
+    nTrials.sort()
+    stats=getMeanStdError(nSims0=1000, nSims1=100, nTrials=nTrials, stdSR=1)
