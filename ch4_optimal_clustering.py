ec9cbcfe273d3b0e464792ccf440d2956a4bb7a0
emoen <endremoen@hotmail.com>
2021-01-07 14:15:08 +0100

    bug: n_init = init+1 -- so that different centroid seeds are initialized, as well as n_clusters in KMeans().
    This bug is present in the book - ML for asset-managers in code snippet 4.1,
    its also in the mlfinlab code base of onc.py.
    Without this initialization the search space is greatly reduced.

--- ch4_optimal_clustering.py
+++ ch4_optimal_clustering.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_samples
+from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn.utils import check_random_state
 from scipy.linalg import block_diag
 import matplotlib.pylab as plt
@@ -15,40 +15,48 @@ Detection of False Investment Strategies using Unsupervised Learning Methods
 https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3167017
 '''
 
-#codesnippet 4.1
-#base clustering
-# kmeans.labels_ is the assignment of members to the cluster
-# [0 1 1 0 0]
-# [1 0 0 1 1] is equivelant
-#Note that the order of the cluster labels for the first two data objects was flipped. 
-#The order was [1, 0] in true_labels but [0, 1] in kmeans.labels_ even though those data objects are still 
-#members of their original clusters in kmeans.lables_.
-#This behavior is normal, as the ordering of cluster labels is dependent on the initialization. 
-#Cluster 0 from the first run could be labeled cluster 1 in the second run and vice versa. 
-#This doesn’t affect clustering evaluation metrics.      
-def clusterKMeansBase(corr0, maxNumClusters=10, n_init=10): 
+'''codesnippet 4.1
+ base clustering: Evaluate the correlation matrix as distance matrix,
+ the find cluster; in the inner loop, we try different k=2..N
+ on which to cluster with kmeans for one given initialization,
+ and evaluate q = E(silhouette)/std(silhouette) for all clusters.
+ The outer loop repeats inner loop with initializations of
+ _different centroid seeds_
+  
+ kmeans.labels_ is the assignment of members to the cluster
+ [0 1 1 0 0]
+ [1 0 0 1 1] is equivelant
+'''
+def clusterKMeansBase(corr0, maxNumClusters=10, n_init=10, debug=False):
+    corr0[corr0 > 1] = 1
     dist_matrix = ((1-corr0.fillna(0))/2.)**.5
     silh_coef_optimal = pd.Series(dtype='float64') #observations matrixs
+    kmeans = None
     maxNumClusters = min(maxNumClusters, int(np.floor(dist_matrix.shape[0]/2)))
-    print("maxNumClusters:"+str(maxNumClusters))
-    for init in range(1, n_init):
+
+    for init in range(0, n_init):
         for num_clusters in range(2, maxNumClusters+1):
-            kmeans_ = KMeans(n_clusters=num_clusters, n_init=1) #n_jobs=None, n_init=1) #n_jobs=None - use all CPUs
+            kmeans_ = KMeans(n_clusters=(maxNumClusters+2-num_clusters), n_init=init+1, random_state=3425) #n_jobs=None, n_init=1) #n_jobs=None - use all CPUs
             kmeans_ = kmeans_.fit(dist_matrix)
             silh_coef = silhouette_samples(dist_matrix, kmeans_.labels_)
-            
             stat = (silh_coef.mean()/silh_coef.std(), silh_coef_optimal.mean()/silh_coef_optimal.std())
-            
+
             # If this metric better than the previous set as the optimal number of clusters
             if np.isnan(stat[1]) or stat[0] > stat[1]:
                 silh_coef_optimal = silh_coef
-                kmeans = kmeans_              
+                kmeans = kmeans_
+                if debug==True:
+                    print(kmeans)
+                    print(stat)
+                    silhouette_avg = silhouette_score(dist_matrix, kmeans_.labels_)
+                    print("For n_clusters ="+ str(num_clusters)+ "The average silhouette_score is :"+ str(silhouette_avg))
     
     newIdx = np.argsort(kmeans.labels_)
-    print(newIdx)
-    corr1 = corr0.iloc[newIdx] #reorder rows
+    #print(newIdx)
     
+    corr1 = corr0.iloc[newIdx] #reorder rows
     corr1 = corr1.iloc[:, newIdx] #reorder columns
+    
     clstrs = {i:corr0.columns[np.where(kmeans.labels_==i)[0]].tolist() for i in np.unique(kmeans.labels_)} #cluster members
     silh_coef_optimal = pd.Series(silh_coef_optimal, index=dist_matrix.index)
     
@@ -102,13 +110,19 @@ def clusterKMeansTop(corr0: pd.DataFrame, maxNumClusters=None, n_init=10):
     if maxNumClusters == None:
         maxNumClusters = corr0.shape[1]-1
         
-    corr1, clstrs, silh = clusterKMeansBase(corr0, maxNumClusters=min(maxNumClusters, corr0.shape[1]-1), n_init=n_init)
+    corr1, clstrs, silh = clusterKMeansBase(corr0, maxNumClusters=min(maxNumClusters, corr0.shape[1]-1), n_init=1)#n_init)
+    #print("clstrs keys:"+str(clstrs.keys()))
+    #for i in clstrs.keys():
+    #    print("std:"+str(np.std(silh[clstrs[i]])))
+
+    #print(clstrs[1])
+    #print(silh)
     clusterTstats = {i:np.mean(silh[clstrs[i]])/np.std(silh[clstrs[i]]) for i in clstrs.keys()}
     tStatMean = sum(clusterTstats.values())/len(clusterTstats)
     redoClusters = [i for i in clusterTstats.keys() if clusterTstats[i] < tStatMean]
-    print("redo cluster:"+str(redoClusters))
+    #print("redo cluster:"+str(redoClusters))
     if len(redoClusters) <= 1:
-        print("redoCluster <=1:"+str(redoClusters))
+        #print("redoCluster <=1:"+str(redoClusters))
         return corr1, clstrs, silh
     else:
         keysRedo = [j for i in redoClusters for j in clstrs[i]]
