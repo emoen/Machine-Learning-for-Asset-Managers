fe3fb1446128b3aab2acddba973ebbb9546f66a4
emoen <endremoen@hotmail.com>
2021-01-09 23:55:10 +0100

    debugging - but not finding any more errors in the clustering code. (found in MLFinLab's implementation of ONC though).

--- ch4_optimal_clustering.py
+++ ch4_optimal_clustering.py
@@ -31,12 +31,16 @@ def clusterKMeansBase(corr0, maxNumClusters=10, n_init=10, debug=False):
     corr0[corr0 > 1] = 1
     dist_matrix = ((1-corr0.fillna(0))/2.)**.5
     silh_coef_optimal = pd.Series(dtype='float64') #observations matrixs
-    kmeans = None
+    kmeans, stat = None, None
     maxNumClusters = min(maxNumClusters, int(np.floor(dist_matrix.shape[0]/2)))
-
+    print("n_init"+str(n_init))
+    print("maxNumClusters"+str(maxNumClusters))
     for init in range(0, n_init):
-        for num_clusters in range(2, maxNumClusters+1):
-            kmeans_ = KMeans(n_clusters=(maxNumClusters+2-num_clusters), n_init=init+1, random_state=3425) #n_jobs=None, n_init=1) #n_jobs=None - use all CPUs
+        for num_clusters in range(4, maxNumClusters+1):
+            #(maxNumClusters + 2 - num_clusters), n_init=init+1
+            #print("(num_clstrs, init):"+str(num_clusters), end=" ")
+            #print(init+1)
+            kmeans_ = KMeans(n_clusters=num_clusters, n_init=10, random_state=3425) #n_jobs=None, n_init=1) #n_jobs=None - use all CPUs
             kmeans_ = kmeans_.fit(dist_matrix)
             silh_coef = silhouette_samples(dist_matrix, kmeans_.labels_)
             stat = (silh_coef.mean()/silh_coef.std(), silh_coef_optimal.mean()/silh_coef_optimal.std())
@@ -50,13 +54,14 @@ def clusterKMeansBase(corr0, maxNumClusters=10, n_init=10, debug=False):
                     print(stat)
                     silhouette_avg = silhouette_score(dist_matrix, kmeans_.labels_)
                     print("For n_clusters ="+ str(num_clusters)+ "The average silhouette_score is :"+ str(silhouette_avg))
+                    print("********")
     
     newIdx = np.argsort(kmeans.labels_)
     #print(newIdx)
-    
+
     corr1 = corr0.iloc[newIdx] #reorder rows
     corr1 = corr1.iloc[:, newIdx] #reorder columns
-    
+
     clstrs = {i:corr0.columns[np.where(kmeans.labels_==i)[0]].tolist() for i in np.unique(kmeans.labels_)} #cluster members
     silh_coef_optimal = pd.Series(silh_coef_optimal, index=dist_matrix.index)
     
@@ -110,35 +115,38 @@ def clusterKMeansTop(corr0: pd.DataFrame, maxNumClusters=None, n_init=10):
     if maxNumClusters == None:
         maxNumClusters = corr0.shape[1]-1
         
-    corr1, clstrs, silh = clusterKMeansBase(corr0, maxNumClusters=min(maxNumClusters, corr0.shape[1]-1), n_init=1)#n_init)
-    #print("clstrs keys:"+str(clstrs.keys()))
+    corr1, clstrs, silh = clusterKMeansBase(corr0, maxNumClusters=min(maxNumClusters, corr0.shape[1]-1), n_init=10)#n_init)
+    print("clstrs length:"+str(len(clstrs.keys())))
+    print("best clustr:"+str(len(clstrs.keys())))
     #for i in clstrs.keys():
     #    print("std:"+str(np.std(silh[clstrs[i]])))
 
-    #print(clstrs[1])
-    #print(silh)
     clusterTstats = {i:np.mean(silh[clstrs[i]])/np.std(silh[clstrs[i]]) for i in clstrs.keys()}
     tStatMean = sum(clusterTstats.values())/len(clusterTstats)
     redoClusters = [i for i in clusterTstats.keys() if clusterTstats[i] < tStatMean]
     #print("redo cluster:"+str(redoClusters))
-    if len(redoClusters) <= 1:
-        #print("redoCluster <=1:"+str(redoClusters))
+    if len(redoClusters) <= 2:
+        print("If 2 or less clusters have a quality rating less than the average then stop.")
+        print("redoCluster <=1:"+str(redoClusters)+" clstrs len:"+str(len(clstrs.keys())))
         return corr1, clstrs, silh
     else:
         keysRedo = [j for i in redoClusters for j in clstrs[i]]
         corrTmp = corr0.loc[keysRedo, keysRedo]
         tStatMean = np.mean([clusterTstats[i] for i in redoClusters])
         _, clstrs2, _ = clusterKMeansTop(corrTmp, maxNumClusters=min(maxNumClusters, corrTmp.shape[1]-1), n_init=n_init)
+        print("clstrs2.len, stat:"+str(len(clstrs2.keys())))
         #Make new outputs, if necessary
         dict_redo_clstrs = {i:clstrs[i] for i in clstrs.keys() if i not in redoClusters}
         corrNew, clstrsNew, silhNew = makeNewOutputs(corr0, dict_redo_clstrs, clstrs2)
         newTstatMean = np.mean([np.mean(silhNew[clstrsNew[i]])/np.std(silhNew[clstrsNew[i]]) for i in clstrsNew.keys()]) 
         if newTstatMean <= tStatMean: 
-            print("newTstatMean <= tStatMean")
-            return corr1, clstrs, silh 
+            print("newTstatMean <= tStatMean"+str(newTstatMean)+ " (len:newClst)"+str(len(clstrsNew.keys()))+" <= "+str(tStatMean)+ " (len:Clst)"+str(len(clstrs.keys())))
+            return corr1, clstrs, silh
         else: 
-            print("newTstatMean > tStatMean")
+            print("newTstatMean > tStatMean"+str(newTstatMean)+ " (len:newClst)"+str(len(clstrsNew.keys()))
+                  +" > "+str(tStatMean)+ " (len:Clst)"+str(len(clstrs.keys())))
             return corrNew, clstrsNew, silhNew
+            #return corr1, clstrs, silh, stat
              
 # codesnippet 4.3 - utility for monte-carlo simulation
 # Random block correlation matrix creation
