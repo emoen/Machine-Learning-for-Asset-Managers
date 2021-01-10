9b72731d35d60fa22a4a710ef356dca0fff6d86d
emoen <endremoen@hotmail.com>
2021-01-10 22:30:36 +0100

    there is no need for the outer loop in clusterKMeansBase() - the algo is a double loop but the inner loop is in KMeans initializing with 10 different centroids (paper sets n_init=1). This is error in Prado et al(2018) in the "DETECTION OF FALSE INVESTMENT STRATEGIES USING UNSUPERVISED LEARNING METHODS" paper

--- ch4_optimal_clustering.py
+++ ch4_optimal_clustering.py
@@ -27,34 +27,30 @@ https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3167017
  [0 1 1 0 0]
  [1 0 0 1 1] is equivelant
 '''
-def clusterKMeansBase(corr0, maxNumClusters=10, n_init=10, debug=False):
+def clusterKMeansBase(corr0, maxNumClusters=10, debug=False):
     corr0[corr0 > 1] = 1
     dist_matrix = ((1-corr0.fillna(0))/2.)**.5
     silh_coef_optimal = pd.Series(dtype='float64') #observations matrixs
     kmeans, stat = None, None
     maxNumClusters = min(maxNumClusters, int(np.floor(dist_matrix.shape[0]/2)))
-    print("n_init"+str(n_init))
     print("maxNumClusters"+str(maxNumClusters))
-    for init in range(0, n_init):
-        for num_clusters in range(4, maxNumClusters+1):
-            #(maxNumClusters + 2 - num_clusters), n_init=init+1
-            #print("(num_clstrs, init):"+str(num_clusters), end=" ")
-            #print(init+1)
-            kmeans_ = KMeans(n_clusters=num_clusters, n_init=10, random_state=3425) #n_jobs=None, n_init=1) #n_jobs=None - use all CPUs
-            kmeans_ = kmeans_.fit(dist_matrix)
-            silh_coef = silhouette_samples(dist_matrix, kmeans_.labels_)
-            stat = (silh_coef.mean()/silh_coef.std(), silh_coef_optimal.mean()/silh_coef_optimal.std())
+    for num_clusters in range(4, maxNumClusters+1):
+        #(maxNumClusters + 2 - num_clusters) # go in reverse order to view more sub-optimal solutions
+        kmeans_ = KMeans(n_clusters=num_clusters, n_init=10, random_state=3425) #n_jobs=None #n_jobs=None - use all CPUs
+        kmeans_ = kmeans_.fit(dist_matrix)
+        silh_coef = silhouette_samples(dist_matrix, kmeans_.labels_)
+        stat = (silh_coef.mean()/silh_coef.std(), silh_coef_optimal.mean()/silh_coef_optimal.std())
 
-            # If this metric better than the previous set as the optimal number of clusters
-            if np.isnan(stat[1]) or stat[0] > stat[1]:
-                silh_coef_optimal = silh_coef
-                kmeans = kmeans_
-                if debug==True:
-                    print(kmeans)
-                    print(stat)
-                    silhouette_avg = silhouette_score(dist_matrix, kmeans_.labels_)
-                    print("For n_clusters ="+ str(num_clusters)+ "The average silhouette_score is :"+ str(silhouette_avg))
-                    print("********")
+        # If this metric better than the previous set as the optimal number of clusters
+        if np.isnan(stat[1]) or stat[0] > stat[1]:
+            silh_coef_optimal = silh_coef
+            kmeans = kmeans_
+            if debug==True:
+                print(kmeans)
+                print(stat)
+                silhouette_avg = silhouette_score(dist_matrix, kmeans_.labels_)
+                print("For n_clusters ="+ str(num_clusters)+ "The average silhouette_score is :"+ str(silhouette_avg))
+                print("********")
     
     newIdx = np.argsort(kmeans.labels_)
     #print(newIdx)
